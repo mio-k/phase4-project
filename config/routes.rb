d49a7db66c4f011a675c2db6cc28745111f33ee0
mio kataoka <mio.kanna@gmail.com>
2022-09-06 16:03:34 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get "/me", to: "users#show"
+  resources :dogs
+  resources :items
+  resources :tags, only: [:index]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
