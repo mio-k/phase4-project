1e17687bca7b379baa4c92b4297ff57d4bc56268
mio kataoka <mio.kanna@gmail.com>
2022-09-09 19:04:52 -0700

    CRUD setup for Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,32 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items
+    end
+    def show
+        item = find_item
+        render json: item
+    end
+    def create
+        item = Item.create!(item_params)
+        render json: item, status: :created
+    end
+    def update
+        item = find_item
+        item.update(item_params)
+        render json: item
+    end
+    def destroy
+        item = find_item
+        item.destroy
+        head :no_content
+    end
+
+    private
+    def find_item
+        Item.find(params[:id])
+    end
+    def item_params
+        params.permit(:name, :description, :user_id)
+    end
 end
