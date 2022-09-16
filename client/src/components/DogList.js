169f1e31dee14d89607a90f40e20b82a5a552ebb
mio kataoka <mio.kanna@gmail.com>
2022-09-15 20:14:43 -0700

    test

--- client/src/components/DogList.js
+++ client/src/components/DogList.js
@@ -10,21 +10,18 @@ function DogList() {
   }, []);
   
     return (
-      <div className="list">
-      <h2>Member Dogs</h2>
-      <p>Choose a dog to drill down to their record.</p>
-      <ul>
+      <div>
+        <ul>
         {dogs.map((dog) => (
           <Link
           style={{ display: "block", margin: "1rem 0" }}
           to={`/dogs/${dog.id}`}
           key={dog.id}
-          dog={dog}
           >
           <li>{dog.name}</li>
         </Link>
-        ))} 
-      </ul>
+        ))}
+        </ul>
       <Outlet />
     </div>
     )
